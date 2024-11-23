23c15317c803646dcd084a6eac88918c775cb74d
Lucas da Silva Oliveira <107443575+LucasdaSilvaOliveira@users.noreply.github.com>
2024-11-23 17:02:57 -0300

    feat: Implementação da lista de projetos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CreateComponent } from './project/create/create.component';
+import { ListProjectsComponent } from './project/list-projects/list-projects.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
     {path: "home", component: HomeComponent},
     {path: "project/create", component: CreateComponent},
+    {path: "project/list-projects", component: ListProjectsComponent},
 ];
