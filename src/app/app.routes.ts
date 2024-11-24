ec4333ef8f4fdf14bc620681bedfd70f2208cff4
Lucas da Silva Oliveira <107443575+LucasdaSilvaOliveira@users.noreply.github.com>
2024-11-24 17:35:23 -0300

    feat: Adicionando rota no projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CreateComponent } from './project/create/create.component';
 import { ListProjectsComponent } from './project/list-projects/list-projects.component';
+import { ManageComponent } from './project/manage/manage.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
     {path: "home", component: HomeComponent},
     {path: "project/create", component: CreateComponent},
     {path: "project/list-projects", component: ListProjectsComponent},
+    {path: "project/manage", component: ManageComponent},
 ];
