d4f6d6987e96a41306b49d2a88bc3f62ee29ecce
Lucas da Silva Oliveira <107443575+LucasdaSilvaOliveira@users.noreply.github.com>
2024-11-26 20:38:31 -0300

    feat: Implementação de formulário dinâmico

--- src/app/project/manage/manage.component.ts
+++ src/app/project/manage/manage.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { EditComponent } from "../edit/edit.component";
+import { CreateServiceComponent } from "../create-service/create-service.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-manage',
   standalone: true,
-  imports: [EditComponent],
+  imports: [EditComponent, CreateServiceComponent, CommonModule],
   templateUrl: './manage.component.html',
   styleUrl: './manage.component.scss'
 })
 export class ManageComponent {
+  toggleEditProjectForms: boolean = false;
+  toggleServiceForms:boolean = false;
 
+  ToggleServiceForms() {
+    this.toggleServiceForms = !this.toggleServiceForms; 
+  }
+
+  ToggleEditProjectForms() {
+    this.toggleEditProjectForms = !this.toggleEditProjectForms;
+  }
 }
