6c9ada7207f66be59906771b3187fe013373eb8d
Lucas da Silva Oliveira <107443575+LucasdaSilvaOliveira@users.noreply.github.com>
2024-12-01 17:19:29 -0300

    feat: start of API route communication

---
+++ src/shared/repository/project-repository.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@angular/core";
+import { Variables } from "../variables";
+
+@Injectable({ providedIn: 'root' })
+export class ProjectRepository {
+
+    // private URL: string;
+    // constructor() {
+    //     this.URL = new Variables().BaseURL; 
+    // }
+
+}
---
+++ src/shared/variables.ts
@@ -0,0 +1,6 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({ providedIn: 'root' })
+export class Variables {
+    BaseURL: string = "https://localhost:7154/";
+}
