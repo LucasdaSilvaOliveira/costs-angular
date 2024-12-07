32dc0796733517be3e57df7c80128867274b47a3
Lucas da Silva Oliveira <107443575+LucasdaSilvaOliveira@users.noreply.github.com>
2024-12-07 14:52:54 -0300

    feat: ProjectById route query implementation in the manage module

--- src/app/project/manage/manage.component.ts
+++ src/app/project/manage/manage.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { EditComponent } from "../edit/edit.component";
 import { CreateServiceComponent } from "../create-service/create-service.component";
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { ProjectRepository } from '../../../shared/repository/project-repository';
+import { Project } from '../../../dtos/project';
 
 @Component({
   selector: 'app-manage',
@@ -10,12 +13,28 @@ import { CommonModule } from '@angular/common';
   templateUrl: './manage.component.html',
   styleUrl: './manage.component.scss'
 })
-export class ManageComponent {
+export class ManageComponent implements OnInit {
   toggleEditProjectForms: boolean = false;
-  toggleServiceForms:boolean = false;
+  toggleServiceForms: boolean = false;
+  project!: Project;
+
+  private activatedRoute = inject(ActivatedRoute);
+  id = this.activatedRoute.snapshot.params['id'];
+
+  constructor(private repository: ProjectRepository) { }
+
+  ngOnInit(): void {
+    this.repository.GetProjectById(this.id).subscribe(
+      {
+        next: (data => {
+          this.project = data;
+        })
+      }
+    );
+  }
 
   ToggleServiceForms() {
-    this.toggleServiceForms = !this.toggleServiceForms; 
+    this.toggleServiceForms = !this.toggleServiceForms;
   }
 
   ToggleEditProjectForms() {
